721c8c1928dc83b28e4593ecb06a8cecbe8540b9
Jim Suchy <jsuchy@gmail.com>
2013-01-23 17:53:39 -0600

    Refactors Teams tests to use @Before.

--- Teams/src/TeamBuilder.java
+++ Teams/src/TeamBuilder.java
@@ -11,16 +11,22 @@ public class TeamBuilder {
 	public List<List<String>> build(int teamSize, String ...players) {
 		List<List<String>> teams = new LinkedList<List<String>>();
 		
-		for (int i=0; i < players.length / teamSize; i++) {
-			int start = i * teamSize;
-			String[] members = Arrays.copyOfRange(players, start, start + teamSize);
-			List<String> team = buildTeam(members);
-			if (team.size() > 0) {
-				teams.add(team);
+		if (isEvenTeams(teamSize, players)) {
+			for (int i=0; i < players.length / teamSize; i++) {
+				int start = i * teamSize;
+				String[] members = Arrays.copyOfRange(players, start, start + teamSize);
+				List<String> team = buildTeam(members);
+				if (team.size() > 0) {
+					teams.add(team);
+				}
 			}
 		}
 		
 		return teams;
+	}
+	
+	private boolean isEvenTeams(int teamSize, String[] players) {
+		return players.length % teamSize == 0;
 	}
 	
 	private List<String> buildTeam(String[] players) {
--- Teams/src/TeamBuilderTest.java
+++ Teams/src/TeamBuilderTest.java
@@ -2,49 +2,70 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TeamBuilderTest {
 
+	private TeamBuilder teamBuilder;
+
+	@Before
+	public void testSetup() {
+		teamBuilder = new TeamBuilder();
+	}
+	
 	@Test
 	public void testNoPlayers() {
-		List<String> expected = new ArrayList<String>();
-		assertEquals(expected, new TeamBuilder().build(1));
+		List<List<String>> teams = teamBuilder.build(1);
+		checkNumberOfTeams(0, teams);
+		checkNumberOfMembersInTeams(0, teams);
 	}
 	
 	@Test
 	public void testOnePlayerSplitIntoTeamOfOne() {
-		List<List<String>> teams = new TeamBuilder().build(1, "Mike");
-		assertEquals(1, teams.size());
-		for (List<String> team : teams) {
-			assertEquals(1, team.size());
-		}
+		List<List<String>> teams = teamBuilder.build(1, "Mike");
+		checkNumberOfTeams(1, teams);
+		checkNumberOfMembersInTeams(1, teams);
 	}
 	
 	@Test
 	public void testTwoPlayersSplitIntoTeamOfTwo() {
-		List<List<String>> teams = new TeamBuilder().build(2, "Mike", "Jim");
-		assertEquals(1, teams.size());
-		for (List<String> team : teams) {
-			assertEquals(2, team.size());
-		}
+		List<List<String>> teams = teamBuilder.build(2, "Mike", "Jim");
+		checkNumberOfTeams(1, teams);
+		checkNumberOfMembersInTeams(2, teams);
 	}
 	
 	@Test
 	public void testFourPlayersSplitIntoTeamsOfTwo() {
-		List<List<String>> teams = new TeamBuilder().build(2, "Mike", "Jim", "Micah", "Bobo");
-		assertEquals(2, teams.size());
-		for (List<String> team : teams) {
-			assertEquals(2, team.size());
-		}
+		List<List<String>> teams = teamBuilder.build(2, "Mike", "Jim", "Micah", "Bobo");
+		checkNumberOfTeams(2, teams);
+		checkNumberOfMembersInTeams(2, teams);
+	}
+	
+	@Test
+	public void testUnevenTeams() {
+		List<List<String>> teams = teamBuilder.build(2, "Mike", "Jim", "Micah");
+		checkNumberOfTeams(0, teams);
+		checkNumberOfMembersInTeams(0, teams);
 	}
 	
 	@Test
 	public void testSixPlayersSplitIntoTeamsOfThree() {
-		List<List<String>> teams = new TeamBuilder().build(3, "Mike", "Jim", "Micah", "Bobo", "Willow", "Franklin");
-		assertEquals(2, teams.size());
+		List<List<String>> teams = teamBuilder.build(3, "Mike", "Jim", "Micah", "Bobo", "Willow", "Franklin");
+		checkNumberOfMembersInTeams(3, teams);
 		for (List<String> team : teams) {
 			assertEquals(3, team.size());
+		}
+	}
+	
+	private void checkNumberOfTeams(int expected, List<List<String>> teams) {
+		assertEquals(expected, teams.size());
+	}
+	private void checkNumberOfMembersInTeams(int expected, List<List<String>> teams) {
+		for (List<String> team : teams) {
+			assertEquals(expected, team.size());
 		}
 	}
 }
