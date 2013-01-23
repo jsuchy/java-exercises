1ccc28a14941b19ca2e9f2268e388664e8f4398b
Jim Suchy <jsuchy@gmail.com>
2013-01-22 23:37:10 -0600

    All tests passing for Teams.

--- Teams/src/TeamBuilder.java
+++ Teams/src/TeamBuilder.java
@@ -5,11 +5,20 @@ public class TeamBuilder {
 
 	public static List<List<String>> build(int teamSize, String ...players) {
 		List<List<String>> teams = new LinkedList<List<String>>();
+		List<String> team = new LinkedList<String>();
+		
 		for (String player : players) {
-			List<String> team = new LinkedList<String>();
 			team.add(player);
-			teams.add(team);
+			if (team.size() == teamSize) {
+				teams.add(team);
+				team = new LinkedList<String>();
+			}
 		}
+		
+		if (team.size() > 0) {
+			teams.add(team);	
+		}
+		
 		return teams;
 	}
 }
--- Teams/src/TeamBuilderTest.java
+++ Teams/src/TeamBuilderTest.java
@@ -7,14 +7,44 @@ import org.junit.Test;
 public class TeamBuilderTest {
 
 	@Test
-	public void testEmptyPlayers() {
+	public void testNoPlayers() {
 		List<String> expected = new ArrayList<String>();
 		assertEquals(expected, TeamBuilder.build(1));
 	}
 	
 	@Test
-	public void testOnePlayersForOneTeam() {
+	public void testOnePlayerSplitIntoTeamOfOne() {
 		List<List<String>> teams = TeamBuilder.build(1, "Mike");
 		assertEquals(1, teams.size());
+		for (List<String> team : teams) {
+			assertEquals(1, team.size());
+		}
+	}
+	
+	@Test
+	public void testTwoPlayersSplitIntoTeamOfTwo() {
+		List<List<String>> teams = TeamBuilder.build(2, "Mike", "Jim");
+		assertEquals(1, teams.size());
+		for (List<String> team : teams) {
+			assertEquals(2, team.size());
+		}
+	}
+	
+	@Test
+	public void testFourPlayersSplitIntoTeamsOfTwo() {
+		List<List<String>> teams = TeamBuilder.build(2, "Mike", "Jim", "Micah", "Bobo");
+		assertEquals(2, teams.size());
+		for (List<String> team : teams) {
+			assertEquals(2, team.size());
+		}
+	}
+	
+	@Test
+	public void testSixPlayersSplitIntoTeamsOfThree() {
+		List<List<String>> teams = TeamBuilder.build(3, "Mike", "Jim", "Micah", "Bobo", "Willow", "Franklin");
+		assertEquals(2, teams.size());
+		for (List<String> team : teams) {
+			assertEquals(3, team.size());
+		}
 	}
 }
