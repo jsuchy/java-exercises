b4a783cc2cadd9431a7107815cacb6973ec73984
Jim Suchy <jsuchy@gmail.com>
2013-01-31 08:24:06 -0600

    First green decision table for dvr AT.

--- DVR/src/com/jsuchy/example/dvr/fixtures/CreatePrograms.java
+++ DVR/src/com/jsuchy/example/dvr/fixtures/CreatePrograms.java
@@ -1,5 +1,34 @@
 package com.jsuchy.example.dvr.fixtures;
 
 public class CreatePrograms {
+	
+	private String name;
+	private int channel;
+	private String dayOfWeek;
+	private String timeOfDay;
+	private int durationInMinutes;
 
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	
+	public void setDayOfWeek(String dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+	
+	public void setTimeOfDay(String timeOfDay) {
+		this.timeOfDay = timeOfDay;
+	}
+	
+	public void setDurationInMinutes(int durationInMinutes) {
+		this.durationInMinutes = durationInMinutes;
+	}
+	
+	public String id() {
+		return String.format("[%s:%d]", name, channel);
+	}
 }
