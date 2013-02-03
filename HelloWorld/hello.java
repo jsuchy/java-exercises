7d3e0103e4b9b870584ec79dcb1e2a8549976ee4
Jim Suchy <jsuchy@gmail.com>
2013-02-03 11:56:10 -0600

    HelloWorld example without IDE.

---
+++ HelloWorld/hello.java
@@ -0,0 +1,6 @@
+/**
+ ** Hi everyone!
+ **/
+
+public class Hello {
+}
---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Hello everybody!
+ *
+ * @author Jim Suchy
+ */
+public class Hello {
+  /**
+   * Says hello.
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello Northampton!");
+  }
+}
